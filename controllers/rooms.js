6e120b4cb7ab176b5d6c6c1c18cfb8ce5ad267d4
kremlevmax <kremlevmax1989@gmail.com>
2022-09-21 23:42:15 -0700

    Add AsyncWrapper

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -1,23 +1,20 @@
 const Room = require("../models/Room");
+const asyncWrapper = require("../middleware/async");
 
-const getAllRooms = async (req, res) => {
+const getAllRooms = asyncWrapper(async (req, res) => {
   const rooms = await Room.find({});
   res.status(200).json({ rooms });
-};
+});
 
-const createRoom = async (req, res) => {
-  try {
-    const room = await Room.create(req.body);
-    res.status(201).json({ room });
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-};
+const createRoom = asyncWrapper(async (req, res) => {
+  const room = await Room.create(req.body);
+  res.status(201).json({ room });
+});
 
-const getRoom = async (req, res) => {
+const getRoom = asyncWrapper(async (req, res) => {
   const { color: roomColor } = req.params;
   const rooms = await Room.find({ colors: { $all: [roomColor] } });
   res.status(200).json({ rooms });
-};
+});
 
 module.exports = { getAllRooms, getRoom, createRoom };
